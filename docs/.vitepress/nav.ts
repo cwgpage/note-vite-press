9e7da3a0a5c2eed107cc6cafd22fbc11b0a98b44
codecwg <501615956@qq.com>
2026-05-08 20:46:39 +0800

    更新

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -2,9 +2,18 @@ const nav = [
   {
     text: '推荐',
     items: [
-      {text: 'Markdown Examples', link: '/markdown-examples'},
-      {text: 'Runtime API Examples', link: '/api-examples'}
+      {text: '应用', link: '/markdown-examples'},
+      {text: '浏览器插件', link: '/api-examples'},
+      {text: 'VSCode插件', link: '/api-examples'},
     ]
+  },
+  {
+    text: '教程',
+    link: '/note/tutorial/index',
+  },
+  {
+    text: '学习',
+    link: '/markdown-examples',
   },
   {text: 'Examples', link: '/markdown-examples'}
 
--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -1,12 +1,32 @@
-const sidebar = [
-  {
-    text: 'Examples',
-    items: [
-      {text: 'Markdown Examples', link: '/markdown-examples'},
-      {text: 'Runtime API Examples', link: '/api-examples'}
-    ]
-  }
-]
+const sidebar = {
+  "/tutorial/": [
+    {
+      text: 'Examples',
+      collapsed: false,
+      items: [
+        {text: '1', link: '/tutorial/index'},
+        {text: '2', link: '/api-examples'}
+      ]
+    },
+    {
+      text: '哈哈啊哈哈哈哈',
+      collapsed: false,
+      items: [
+        {text: '1', link: '/tutorial/index'},
+        {text: '2', link: '/api-examples'}
+      ]
+    },
+    {
+      text: '哈哈啊哈哈哈哈',
+      link: '/api-examples'
+      // collapsed: false,
+      // items: [
+      //   {text: '1', link: '/note/tutorial/index'},
+      //   {text: '2', link: '/api-examples'}
+      // ]
+    }
+  ]
+}
 
 
 export default sidebar
